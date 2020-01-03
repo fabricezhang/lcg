9ac63a56d81844da81e8a7ee7b91a0fac2cee74a
fabricezhang <fabrice@163.com>
2020-01-03 14:56:38 +0800

    fix: bypass ssl pinning when debug

--- app/src/main/java/top/easelink/lcg/network/Client.kt
+++ app/src/main/java/top/easelink/lcg/network/Client.kt
@@ -1,5 +1,7 @@
 package top.easelink.lcg.network
 
+import android.annotation.SuppressLint
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -13,8 +15,17 @@ import top.easelink.lcg.utils.WebsiteConstant.SERVER_BASE_URL
 import top.easelink.lcg.utils.getCookies
 import top.easelink.lcg.utils.setCookies
 import java.net.SocketTimeoutException
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
 
 object Client {
+
+    init {
+        if (BuildConfig.DEBUG) {
+            trustAll()
+        }
+    }
 
     private var lastTime = 0L
     private val CHECK_INTERVAL = if (BuildConfig.DEBUG) 60 * 1000 else 30 * 1000
@@ -61,6 +72,43 @@ object Client {
             lastTime = System.currentTimeMillis()
             checkLoginState(doc)
             checkMessages(doc)
+        }
+    }
+
+    private fun trustAll() {
+        try { // Create a trust manager that does not validate certificate chains
+            val trustAllCerts =
+                arrayOf<TrustManager>(object : X509TrustManager {
+                    override fun getAcceptedIssuers(): Array<X509Certificate>? {
+                        return null
+                    }
+
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkClientTrusted(
+                        certs: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    @SuppressLint("TrustAllX509TrustManager")
+                    override fun checkServerTrusted(
+                        certs: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+                }
+                )
+            // Install the all-trusting trust manager
+            val sc = SSLContext.getInstance("SSL")
+            sc.init(null, trustAllCerts, SecureRandom())
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.socketFactory)
+            // Create all-trusting host name verifier
+            val allHostsValid =
+                HostnameVerifier { hostname, session -> true }
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid)
+        } catch (e: Exception) {
+            Log.e("SSL", e.message, e)
         }
     }
 }
