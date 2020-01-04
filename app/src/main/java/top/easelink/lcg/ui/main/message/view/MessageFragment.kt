cedfdba833ae7ec5229fe8bdb9f8cf506b4bcb63
fabricezhang <fabrice@163.com>
2020-01-04 22:15:17 +0800

    update: update login instruction && add login check for message fragment

--- app/src/main/java/top/easelink/lcg/ui/main/message/view/MessageFragment.kt
+++ app/src/main/java/top/easelink/lcg/ui/main/message/view/MessageFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.fragment_message.*
 import top.easelink.framework.topbase.ControllableFragment
 import top.easelink.framework.topbase.TopFragment
 import top.easelink.lcg.R
+import top.easelink.lcg.ui.info.UserData
+import top.easelink.lcg.ui.main.login.view.LoginHintDialog
 import top.easelink.lcg.ui.main.message.viewmodel.MessageViewPagerAdapter
 
 class MessageFragment: TopFragment(), ControllableFragment{
@@ -22,7 +26,13 @@ class MessageFragment: TopFragment(), ControllableFragment{
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        message_view_pager.adapter = MessageViewPagerAdapter(childFragmentManager, mContext)
-        message_tab.setupWithViewPager(message_view_pager)
+        if (!UserData.loggedInState) {
+            (mContext as? AppCompatActivity)?.let {
+                LoginHintDialog().show(it.supportFragmentManager, null)
+            }
+        } else {
+            message_view_pager.adapter = MessageViewPagerAdapter(childFragmentManager, mContext)
+            message_tab.setupWithViewPager(message_view_pager)
+        }
     }
 }
