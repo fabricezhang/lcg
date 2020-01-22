e4d2eeae65d12ac65e4f3b542f0270fba2519a40
fabricezhang <fabrice@163.com>
2020-01-22 15:52:54 +0800

    fix: fix search bar issue

--- app/src/main/java/top/easelink/lcg/ui/main/recommand/view/RecommendFragment.kt
+++ app/src/main/java/top/easelink/lcg/ui/main/recommand/view/RecommendFragment.kt
@@ -1,15 +1,18 @@
 package top.easelink.lcg.ui.main.recommand.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import kotlinx.android.synthetic.main.fragment_recommand.*
 import top.easelink.framework.topbase.ControllableFragment
+import top.easelink.framework.topbase.TopActivity
 import top.easelink.framework.topbase.TopFragment
 import top.easelink.lcg.R
 import top.easelink.lcg.ui.main.recommand.viewmodel.RecommendViewPagerAdapter
+import top.easelink.lcg.ui.search.view.SearchActivity
+import top.easelink.lcg.utils.WebsiteConstant.SEARCH_URL
+import top.easelink.lcg.utils.WebsiteConstant.URL_KEY
 
 
 class RecommendFragment: TopFragment(), ControllableFragment {
@@ -36,35 +39,29 @@ class RecommendFragment: TopFragment(), ControllableFragment {
 
         view_pager.adapter = RecommendViewPagerAdapter(childFragmentManager, activity)
         main_tab.setupWithViewPager(view_pager)
+        (activity as TopActivity).setSupportActionBar(toolbar)
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//        menu.clear()
-//        inflater.inflate(R.menu.search, menu)
-//        val item = menu.findItem(R.id.search)
-//        val searchView = SearchView((mContext as TopActivity).supportActionBar?.themedContext)
-//        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW)
-//        item.actionView = searchView
-//        searchView.apply {
-//            isSubmitButtonEnabled = true
-//            setOnQueryTextListener(object :
-//                SearchView.OnQueryTextListener {
-//                override fun onQueryTextSubmit(query: String): Boolean {
-//                    if (context != null) {
-//                        val intent = Intent(context, SearchActivity::class.java)
-//                        intent.putExtra(WebsiteConstant.URL_KEY, String.format(WebsiteConstant.SEARCH_URL, query))
-//                        context?.startActivity(intent)
-//                    }
-//                    return true
-//                }
-//                override fun onQueryTextChange(newText: String): Boolean {
-//                    return false
-//                }
-//            })
-//        }
-//        return super.onCreateOptionsMenu(menu, inflater)
-//    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        menu.clear()
+        inflater.inflate(R.menu.search, menu)
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val intent = Intent(context, SearchActivity::class.java)
+                intent.putExtra(URL_KEY, java.lang.String.format(SEARCH_URL, query))
+                context?.startActivity(intent)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (item.itemId == R.id.search) {
