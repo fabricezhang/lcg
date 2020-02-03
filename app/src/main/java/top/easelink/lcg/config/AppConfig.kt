f014b73e4ae90a3eb6d36d126639097e88419dad
fabricezhang <fabrice@163.com>
2020-02-03 22:57:19 +0800

    fix: fix wrong config key

--- app/src/main/java/top/easelink/lcg/config/AppConfig.kt
+++ app/src/main/java/top/easelink/lcg/config/AppConfig.kt
@@ -4,7 +4,7 @@ import com.tencent.stat.StatConfig
 
 object AppConfig {
 
-    private const val CONFIG_APP_RELEASE_URL = "app_release_url"
+    private const val CONFIG_APP_RELEASE_URL = "app_release_page"
 
     fun getAppReleaseUrl(): String {
         return StatConfig.getCustomProperty(CONFIG_APP_RELEASE_URL, "thread-1073834-1-1.html")
