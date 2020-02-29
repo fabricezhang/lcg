cde7a43c5155605fdd9e694d1a39ce40a3055974
FabriceZhang <fabrice@163.com>
2020-02-29 22:59:47 +0800

    fix: fix wrong commented code

--- app/src/main/java/top/easelink/lcg/ui/main/article/viewmodel/ReplyPostViewModel.kt
+++ app/src/main/java/top/easelink/lcg/ui/main/article/viewmodel/ReplyPostViewModel.kt
@@ -4,8 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.jsoup.Connection
+import org.jsoup.Jsoup
 import timber.log.Timber
 import top.easelink.framework.threadpool.ApiPool
+import top.easelink.lcg.network.Client
+import top.easelink.lcg.utils.WebsiteConstant.CHECK_RULE_URL
+import top.easelink.lcg.utils.WebsiteConstant.SERVER_BASE_URL
+import top.easelink.lcg.utils.getCookies
+import top.easelink.lcg.utils.setCookies
 
 class ReplyPostViewModel : ViewModel() {
 
@@ -36,53 +43,51 @@ class ReplyPostViewModel : ViewModel() {
                 }
             }
         try {
-            callback.invoke(true)
-//            Client
-//                .sendGetRequestWithQuery(query)
-//                .run {
-//                    val noticeauthormsg = selectFirst("input[name=noticeauthormsg]").attr("value")
-//                    val noticetrimstr = selectFirst("input[name=noticetrimstr]").attr("value")
-//                    val noticeauthor = selectFirst("input[name=noticeauthor]").attr("value")
-//                    val handlekey = selectFirst("input[name=handlekey]")?.attr("value")?:"reply"
-//                    val usesig = selectFirst("input[name=usesig]")?.attr("value")?:"1"
-//                    val reppid = selectFirst("input[name=reppid]").attr("value")
-//                    val reppost = selectFirst("input[name=reppost]").attr("value")
-//                    val formHash = selectFirst("input[name=formhash]").attr("value")
-//                    var response = Jsoup
-//                        .connect(CHECK_RULE_URL)
-//                        .timeout(60* 1000)
-//                        .cookies(getCookies())
-//                        .method(Connection.Method.GET)
-//                        .execute()
-//                    if (response.statusCode() in 200 until 300) {
-//                        setCookies(response.cookies())
-//                        val url = "${SERVER_BASE_URL}forum.php?mod=post&infloat=yes&action=reply" +
-//                                "&fid=${queryMap["fid"]}&extra=${queryMap["extra"]}&tid=${queryMap["tid"]}&replysubmit=yes&inajax=1"
-//                        response = Jsoup
-//                            .connect(url)
-//                            .cookies(getCookies())
-//                            .data(
-//                                "formhash", formHash,
-//                                "handlekey", handlekey,
-//                                "noticeauthor", noticeauthor,
-//                                "noticetrimstr", noticetrimstr,
-//                                "noticeauthormsg", noticeauthormsg,
-//                                "usesig", usesig,
-//                                "reppid", reppid,
-//                                "reppost", reppost,
-//                                "subject", "",
-//                                "message", content
-//                            )
-//                            .postDataCharset("gbk")
-//                            .method(Connection.Method.POST)
-//                            .execute()
-//                        setCookies(response.cookies())
-//                        callback.invoke(response.statusCode() in 200 until 300)
-//                    } else {
-//                        Timber.e(response.body())
-//                    }
-//
-//                }
+            Client
+                .sendGetRequestWithQuery(query)
+                .run {
+                    val noticeauthormsg = selectFirst("input[name=noticeauthormsg]").attr("value")
+                    val noticetrimstr = selectFirst("input[name=noticetrimstr]").attr("value")
+                    val noticeauthor = selectFirst("input[name=noticeauthor]").attr("value")
+                    val handlekey = selectFirst("input[name=handlekey]")?.attr("value")?:"reply"
+                    val usesig = selectFirst("input[name=usesig]")?.attr("value")?:"1"
+                    val reppid = selectFirst("input[name=reppid]").attr("value")
+                    val reppost = selectFirst("input[name=reppost]").attr("value")
+                    val formHash = selectFirst("input[name=formhash]").attr("value")
+                    var response = Jsoup
+                        .connect(CHECK_RULE_URL)
+                        .timeout(60* 1000)
+                        .cookies(getCookies())
+                        .method(Connection.Method.GET)
+                        .execute()
+                    if (response.statusCode() in 200 until 300) {
+                        setCookies(response.cookies())
+                        val url = "${SERVER_BASE_URL}forum.php?mod=post&infloat=yes&action=reply" +
+                                "&fid=${queryMap["fid"]}&extra=${queryMap["extra"]}&tid=${queryMap["tid"]}&replysubmit=yes&inajax=1"
+                        response = Jsoup
+                            .connect(url)
+                            .cookies(getCookies())
+                            .data(
+                                "formhash", formHash,
+                                "handlekey", handlekey,
+                                "noticeauthor", noticeauthor,
+                                "noticetrimstr", noticetrimstr,
+                                "noticeauthormsg", noticeauthormsg,
+                                "usesig", usesig,
+                                "reppid", reppid,
+                                "reppost", reppost,
+                                "subject", "",
+                                "message", content
+                            )
+                            .postDataCharset("gbk")
+                            .method(Connection.Method.POST)
+                            .execute()
+                        setCookies(response.cookies())
+                        callback.invoke(response.statusCode() in 200 until 300)
+                    } else {
+                        Timber.e(response.body())
+                    }
+                }
         } catch (e: Exception) {
             Timber.e(e)
             callback.invoke(false)
